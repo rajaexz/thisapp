4027b4cfa2883ba196315bed526f9add6edbb0d5
unknown <deepkexz@gmail.com>
2021-12-28 19:58:23 +0530

    app start

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express');
 const app= express();
 const fs = require("fs");
 const path = require("path");
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(express.urlencoded({extended:true}));
 app.use( express.static(path.join(__dirname, 'public')));
 app.use(express.json()); 
@@ -11,7 +11,7 @@ app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'index.html'))
 })
 
-
+ 
 app.listen(port,()=>{
-    console.log('server is working ');
+    console.log(`server is working ${port}`);
 })
